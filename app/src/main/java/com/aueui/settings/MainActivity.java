8e1753d306ea68c587a5e8471b868b24f18afcfb
yarsict <yarsict@126.com>
2019-05-03 16:37:38 +0800

    1.0.0-B1

--- app/src/main/java/com/aueui/settings/MainActivity.java
+++ app/src/main/java/com/aueui/settings/MainActivity.java
@@ -1,13 +1,28 @@
 package com.aueui.settings;
 
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity  {
+private LinearLayout wifi_l;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        wifi_l.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+            }
+        });
+    }
+    private void initView() {
+        wifi_l=findViewById(R.id.wifi_l);
     }
 }
